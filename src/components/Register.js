6bd20d0cdbadc744a5d035cdaf871daffb32c8a9
Mikhail Ziankevich <zin@zame-dev.org>
2017-08-01 22:09:54 +0200

    Google JS login

--- src/components/Register.js
+++ src/components/Register.js
@@ -21,6 +21,38 @@ class Register extends Component {
 	}
 
 	componentDidMount() {
+		let auth2 = null;
+
+		let startApp = function() {
+			gapi.load('auth2', function() {
+			// Retrieve the singleton for the GoogleAuth library and set up the client.
+			auth2 = gapi.auth2.init({
+				client_id: '663608028282-qngrticdc4d8k24sbejp96l49bgde2uf.apps.googleusercontent.com',
+				cookiepolicy: 'single_host_origin',
+				// Request scopes in addition to 'profile' and 'email'
+				// scope: 'additional_scope'
+			});
+				attachSignin(document.getElementById('customBtn'));
+			});
+		};
+
+		function attachSignin(element) {
+			console.log(element.id);
+			auth2.attachClickHandler(element, {},
+				function(googleUser) {
+					console.log(googleUser.getBasicProfile().getName());
+					console.log(googleUser.getBasicProfile().getEmail());
+					console.log(googleUser.getBasicProfile());
+					}, function(error) {
+						alert(JSON.stringify(error, undefined, 2));
+					});
+		}
+
+		startApp();
+	}
+
+	google(event) {
+		console.log(event.target);
 	}
 
 	render() {
@@ -30,16 +62,33 @@ class Register extends Component {
 					<div className="col-xs-12 col-lg-8 col-lg-offset-2">
 						<h2>Registration</h2>
 
-						<div className="input-group">
-							<span className="input-group-addon" id="addon-filter">Filter</span>
-							<input type="text" className="form-control" placeholder="Some words for filtering..." aria-describedby="addon-filter" />
-						</div>
+						<form>
+							<div className="form-group">
+								<input type="text" className="form-control" placeholder="Your Name" />
+							</div>
+
+							<div className="form-group">
+								<input type="email" className="form-control" placeholder="Your E-mail" />
+							</div>
+
+							<div className="form-group">
+								<input type="password" className="form-control" placeholder="Password" />
+							</div>
+
+							<div className="form-group">
+								<input type="password" className="form-control" placeholder="Repeat password" />
+							</div>
+
+							<button type="submit" className="btn btn-primary">Register</button>
+						</form>
 					</div>
 				</div>
 
 				<div className="row">
+					<hr />
+
 					<div className="col-xs-12 col-lg-3 col-lg-offset-2">
-						<div className="loginBtn--facebook">
+						<div className="loginBtn--facebook" >
 							<span className="loginBtn__buttonText--facebook">Sign In with</span>
 							<span className="loginBtn__icon--facebook"></span>
 						</div>
@@ -48,7 +97,7 @@ class Register extends Component {
 						or
 					</div>
 					<div className="col-xs-12 col-lg-3">
-						<div className="loginBtn--google">
+						<div className="loginBtn--google" id="customBtn" onClick={this.google}>
 							<span className="loginBtn__icon--google"></span>
 							<span className="loginBtn__buttonText--google">Sign In with Google</span>
 						</div>
