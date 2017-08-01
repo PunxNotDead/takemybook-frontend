0f169c5a9ac40d1550e7e62e101cd4aa15976ad0
Mikhail Ziankevich <mikhail.ziankevich@motorolasolutions.com>
2017-08-01 15:33:05 +0200

    Facebook & Google buttons

--- src/components/Register.js
+++ src/components/Register.js
@@ -12,6 +12,8 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => bindActionCreators({ loadBooks }, dispatch);
 
+// https://developers.google.com/identity/branding-guidelines
+
 
 class Register extends Component {
 	constructor(props) {
@@ -31,6 +33,24 @@ class Register extends Component {
 						<div className="input-group">
 							<span className="input-group-addon" id="addon-filter">Filter</span>
 							<input type="text" className="form-control" placeholder="Some words for filtering..." aria-describedby="addon-filter" />
+						</div>
+					</div>
+				</div>
+
+				<div className="row">
+					<div className="col-xs-12 col-lg-3 col-lg-offset-2">
+						<div className="loginBtn--facebook">
+							<span className="loginBtn__buttonText--facebook">Sign In with</span>
+							<span className="loginBtn__icon--facebook"></span>
+						</div>
+					</div>
+					<div className="col-xs-12 col-lg-2 text-center">
+						or
+					</div>
+					<div className="col-xs-12 col-lg-3">
+						<div className="loginBtn--google">
+							<span className="loginBtn__icon--google"></span>
+							<span className="loginBtn__buttonText--google">Sign In with Google</span>
 						</div>
 					</div>
 				</div>
